7b8e71caffed484266526b2f4413c63bbd933db4
jydalton <138138012+jydalton@users.noreply.github.com>
2023-12-27 08:55:24 -0600

    Update streamlit_app.py
    fixed attribute error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.paragraph('Omega 3 & Blueberry Oatmeal \nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal \nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
