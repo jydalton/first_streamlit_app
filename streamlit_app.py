ad108bc598fd5b201de6fa48f5a4e511fccfc673
jydalton <138138012+jydalton@users.noreply.github.com>
2023-12-27 08:56:37 -0600

    Update streamlit_app.py
    re-organized menu items into each attribute

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal \nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
