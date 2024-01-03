c265e515018a0f4ba152aab3eace662cb053c54f
jydalton <138138012+jydalton@users.noreply.github.com>
2024-01-03 15:26:45 -0600

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show) # Display the table on the page.
 
-def get_fruityvice_data(the_fruit_choice): #create repeatable code block/function
+def get_fruityvice_data(this_fruit_choice): #create repeatable code block/function
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
