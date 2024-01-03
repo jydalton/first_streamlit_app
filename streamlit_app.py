f3c2c8fce109046ace7db6fc5d82b15c03e7cb98
jydalton <138138012+jydalton@users.noreply.github.com>
2024-01-03 12:22:49 -0600

    Update streamlit_app.py
    separating based U from fruit mane for easier variable usability

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
 # normalizes jso version of response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
